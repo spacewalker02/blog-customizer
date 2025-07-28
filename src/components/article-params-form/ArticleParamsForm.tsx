d3765e4628b79e32f11c6fde4f08f71e7200e7a2
Svetlana Saykova <svetlanasaykova@gmail.com>
2025-07-29 02:14:22 +0400

    1

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,97 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { backgroundColors, contentWidthArr, fontColors, fontSizeOptions, OptionType } from 'src/constants/articleProps';
+import clsx from 'clsx';
+import { Select } from 'src/ui/select';
+import { fontFamilyOptions } from 'src/constants/articleProps';
+import { Separator } from 'src/ui/separator';
 
 import styles from './ArticleParamsForm.module.scss';
+import { RadioGroup } from 'src/ui/radio-group';
 
-export const ArticleParamsForm = () => {
+type ArticleSetting = {
+	fontFamilyOption: OptionType;
+	fontColor: OptionType;
+	backgroundColor: OptionType;
+	contentWidth: OptionType;
+	fontSizeOption: OptionType;
+};
+
+interface ArticleParamsFormProps {
+	tempSettings: ArticleSetting;
+	setTempSettings: React.Dispatch<React.SetStateAction<ArticleSetting>>;
+	onApply: () => void;
+	onReset: () => void;
+	isOpen: boolean;
+	onClose: () => void;
+}
+
+export const ArticleParamsForm = ({
+	tempSettings,
+	setTempSettings,
+	onApply,
+	onReset,
+	isOpen,
+	onClose,
+}: ArticleParamsFormProps) => {
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton isOpen={isOpen} onClick={onClose} />
+			<aside className={clsx(styles.container, isOpen && styles.container_open)}>
+				<form className={styles.form}
+				onSubmit={(e) => {
+					e.preventDefault();
+					onApply();
+				}}
+				onReset={(e) => {
+					e.preventDefault();
+					onReset();
+				}}
+				>
+				<h2 className={styles.title_h2}>Задайте параметры</h2>
+				<Select
+  					title="шрифт"
+  					selected={tempSettings.fontFamilyOption}
+  					options={fontFamilyOptions}
+  					onChange={(newOption) =>
+    				setTempSettings({ ...tempSettings, fontFamilyOption: newOption })
+  					}
+				/>
+				<RadioGroup
+					title="размер шрифта"
+					name="font-size"
+					selected={tempSettings.fontSizeOption}
+					options={fontSizeOptions}
+					onChange={(newOption) => 
+					setTempSettings({...tempSettings, fontSizeOption: newOption })
+					}
+				/>
+				<Select
+  					title="цвет шрифта"
+  					selected={tempSettings.fontColor}
+  					options={fontColors}
+  					onChange={(newOption) =>
+    				setTempSettings({ ...tempSettings, fontColor: newOption })
+  					}
+				/>
+				<Separator
+				/>
+				<Select
+  					title="цвет фона"
+  					selected={tempSettings.backgroundColor}
+  					options={backgroundColors}
+  					onChange={(newOption) =>
+    				setTempSettings({ ...tempSettings, backgroundColor: newOption })
+  					}
+				/>
+				<Select
+  					title="цвет контента"
+  					selected={tempSettings.contentWidth}
+  					options={contentWidthArr}
+  					onChange={(newOption) =>
+    				setTempSettings({ ...tempSettings, contentWidth: newOption })
+  					}
+				/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
@@ -8,24 +8,37 @@ import { defaultArticleState } from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
+import { ArrowButton } from './ui/arrow-button';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [appliedSettings, setAppliedSettings] = useState(defaultArticleState);
+	const [tempSettings, setTempSettings] = useState(defaultArticleState);
+	const [isSidebarOpen, setSidebarOpen] = useState(false);
+
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': appliedSettings.fontFamilyOption.value,
+					'--font-size': appliedSettings.fontSizeOption.value,
+					'--font-color': appliedSettings.fontColor.value,
+					'--container-width': appliedSettings.contentWidth.value,
+					'--bg-color': appliedSettings.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm 
+			    tempSettings={tempSettings}
+				setTempSettings={setTempSettings}
+				onApply={() => setAppliedSettings(tempSettings)}
+				onReset={() => setTempSettings(appliedSettings)}
+				isOpen={isSidebarOpen}
+				onClose={() => setSidebarOpen(!isSidebarOpen)}
+			/>
+			<ArrowButton isOpen={isSidebarOpen} onClick={() => setSidebarOpen(!isSidebarOpen)} />
 			<Article />
 		</main>
 	);
